19b1bb8d1ec17950ca02e044306a7307faae2bc4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-10-25 02:43:06 +0300

    initial commit

---
+++ CS/T292798/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace T292798 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            DevExpress.DataAccess.Sql.TableQuery tableQuery1 = new DevExpress.DataAccess.Sql.TableQuery();
+            DevExpress.DataAccess.Sql.TableInfo tableInfo1 = new DevExpress.DataAccess.Sql.TableInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo1 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo2 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo3 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo4 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo5 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo6 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo7 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo8 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo9 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo10 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sqlDataSource1 = new DevExpress.DataAccess.Sql.SqlDataSource();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colProductID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colProductName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSupplierID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCategoryID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colQuantityPerUnit = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitPrice = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitsInStock = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUnitsOnOrder = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colReorderLevel = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDiscontinued = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.btConfigureConnection = new System.Windows.Forms.Button();
+            this.btEditQuery = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlDataSource1
+            // 
+            this.sqlDataSource1.ConnectionName = "Northwind";
+            this.sqlDataSource1.Name = "sqlDataSource1";
+            tableQuery1.Name = "DefaultQuery";
+            tableInfo1.Name = "Products";
+            columnInfo1.Name = "ProductID";
+            columnInfo2.Name = "ProductName";
+            columnInfo3.Name = "SupplierID";
+            columnInfo4.Name = "CategoryID";
+            columnInfo5.Name = "QuantityPerUnit";
+            columnInfo6.Name = "UnitPrice";
+            columnInfo7.Name = "UnitsInStock";
+            columnInfo8.Name = "UnitsOnOrder";
+            columnInfo9.Name = "ReorderLevel";
+            columnInfo10.Name = "Discontinued";
+            tableInfo1.SelectedColumns.AddRange(new DevExpress.DataAccess.Sql.ColumnInfo[] {
+            columnInfo1,
+            columnInfo2,
+            columnInfo3,
+            columnInfo4,
+            columnInfo5,
+            columnInfo6,
+            columnInfo7,
+            columnInfo8,
+            columnInfo9,
+            columnInfo10});
+            tableQuery1.Tables.AddRange(new DevExpress.DataAccess.Sql.TableInfo[] {
+            tableInfo1});
+            this.sqlDataSource1.Queries.AddRange(new DevExpress.DataAccess.Sql.SqlQuery[] {
+            tableQuery1});
+            this.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable");
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.gridControl1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btConfigureConnection, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btEditQuery, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(632, 278);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataMember = "DefaultQuery";
+            this.gridControl1.DataSource = this.sqlDataSource1;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(3, 3);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.tableLayoutPanel1.SetRowSpan(this.gridControl1, 3);
+            this.gridControl1.Size = new System.Drawing.Size(495, 272);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colProductID,
+            this.colProductName,
+            this.colSupplierID,
+            this.colCategoryID,
+            this.colQuantityPerUnit,
+            this.colUnitPrice,
+            this.colUnitsInStock,
+            this.colUnitsOnOrder,
+            this.colReorderLevel,
+            this.colDiscontinued});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colProductID
+            // 
+            this.colProductID.FieldName = "ProductID";
+            this.colProductID.Name = "colProductID";
+            this.colProductID.Visible = true;
+            this.colProductID.VisibleIndex = 0;
+            // 
+            // colProductName
+            // 
+            this.colProductName.FieldName = "ProductName";
+            this.colProductName.Name = "colProductName";
+            this.colProductName.Visible = true;
+            this.colProductName.VisibleIndex = 1;
+            // 
+            // colSupplierID
+            // 
+            this.colSupplierID.FieldName = "SupplierID";
+            this.colSupplierID.Name = "colSupplierID";
+            this.colSupplierID.Visible = true;
+            this.colSupplierID.VisibleIndex = 2;
+            // 
+            // colCategoryID
+            // 
+            this.colCategoryID.FieldName = "CategoryID";
+            this.colCategoryID.Name = "colCategoryID";
+            this.colCategoryID.Visible = true;
+            this.colCategoryID.VisibleIndex = 3;
+            // 
+            // colQuantityPerUnit
+            // 
+            this.colQuantityPerUnit.FieldName = "QuantityPerUnit";
+            this.colQuantityPerUnit.Name = "colQuantityPerUnit";
+            this.colQuantityPerUnit.Visible = true;
+            this.colQuantityPerUnit.VisibleIndex = 4;
+            // 
+            // colUnitPrice
+            // 
+            this.colUnitPrice.FieldName = "UnitPrice";
+            this.colUnitPrice.Name = "colUnitPrice";
+            this.colUnitPrice.Visible = true;
+            this.colUnitPrice.VisibleIndex = 5;
+            // 
+            // colUnitsInStock
+            // 
+            this.colUnitsInStock.FieldName = "UnitsInStock";
+            this.colUnitsInStock.Name = "colUnitsInStock";
+            this.colUnitsInStock.Visible = true;
+            this.colUnitsInStock.VisibleIndex = 6;
+            // 
+            // colUnitsOnOrder
+            // 
+            this.colUnitsOnOrder.FieldName = "UnitsOnOrder";
+            this.colUnitsOnOrder.Name = "colUnitsOnOrder";
+            this.colUnitsOnOrder.Visible = true;
+            this.colUnitsOnOrder.VisibleIndex = 7;
+            // 
+            // colReorderLevel
+            // 
+            this.colReorderLevel.FieldName = "ReorderLevel";
+            this.colReorderLevel.Name = "colReorderLevel";
+            this.colReorderLevel.Visible = true;
+            this.colReorderLevel.VisibleIndex = 8;
+            // 
+            // colDiscontinued
+            // 
+            this.colDiscontinued.FieldName = "Discontinued";
+            this.colDiscontinued.Name = "colDiscontinued";
+            this.colDiscontinued.Visible = true;
+            this.colDiscontinued.VisibleIndex = 9;
+            // 
+            // btConfigureConnection
+            // 
+            this.btConfigureConnection.Location = new System.Drawing.Point(504, 3);
+            this.btConfigureConnection.Name = "btConfigureConnection";
+            this.btConfigureConnection.Size = new System.Drawing.Size(125, 23);
+            this.btConfigureConnection.TabIndex = 1;
+            this.btConfigureConnection.Text = "Configure Connection";
+            this.btConfigureConnection.UseVisualStyleBackColor = true;
+            this.btConfigureConnection.Click += new System.EventHandler(this.btConfigureConnection_Click);
+            // 
+            // btEditQuery
+            // 
+            this.btEditQuery.AutoSize = true;
+            this.btEditQuery.Location = new System.Drawing.Point(504, 32);
+            this.btEditQuery.Name = "btEditQuery";
+            this.btEditQuery.Size = new System.Drawing.Size(125, 23);
+            this.btEditQuery.TabIndex = 2;
+            this.btEditQuery.Text = "Edit Query";
+            this.btEditQuery.UseVisualStyleBackColor = true;
+            this.btEditQuery.Click += new System.EventHandler(this.btEditQuery_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btConfigureConnection;
+        private System.Windows.Forms.Button btEditQuery;
+        private DevExpress.DataAccess.Sql.SqlDataSource sqlDataSource1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colProductID;
+        private DevExpress.XtraGrid.Columns.GridColumn colProductName;
+        private DevExpress.XtraGrid.Columns.GridColumn colSupplierID;
+        private DevExpress.XtraGrid.Columns.GridColumn colCategoryID;
+        private DevExpress.XtraGrid.Columns.GridColumn colQuantityPerUnit;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitPrice;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitsInStock;
+        private DevExpress.XtraGrid.Columns.GridColumn colUnitsOnOrder;
+        private DevExpress.XtraGrid.Columns.GridColumn colReorderLevel;
+        private DevExpress.XtraGrid.Columns.GridColumn colDiscontinued;
+
+    }
+}
+
---
+++ CS/T292798/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.DataAccess.UI.Sql;
+
+namespace T292798 {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        public Form1() {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            sqlDataSource1.Fill();
+        }
+
+        private void btConfigureConnection_Click(object sender, EventArgs e) {
+            SqlDataSourceUIHelper.ConfigureConnection(sqlDataSource1);
+        }
+
+        private void btEditQuery_Click(object sender, EventArgs e) {
+            SqlDataSourceUIHelper.EditQuery(sqlDataSource1.Queries["DefaultQuery"]);
+
+            sqlDataSource1.Fill();
+
+            gridView1.PopulateColumns();
+        }
+    }
+}
