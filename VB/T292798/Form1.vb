18aa7d82b0970321bc1d2ed0feb9f4128a773fe1
DevExpressExampleBot <fakeEmail>
2023-12-18 14:43:16 +0400

    Source auto update [skip ci]

--- VB/T292798/Form1.vb
+++ VB/T292798/Form1.vb
@@ -1,34 +1,28 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Windows.Forms
 Imports DevExpress.DataAccess.UI.Sql
 
 Namespace T292798
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             sqlDataSource1.Fill()
         End Sub
 
-        Private Sub btConfigureConnection_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btConfigureConnection.Click
-            SqlDataSourceUIHelper.ConfigureConnection(sqlDataSource1)
+        Private Sub btConfigureConnection_Click(ByVal sender As Object, ByVal e As EventArgs)
+            sqlDataSource1.ConfigureConnection
         End Sub
 
-        Private Sub btEditQuery_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btEditQuery.Click
-            SqlDataSourceUIHelper.EditQuery(sqlDataSource1.Queries("DefaultQuery"))
-
+        Private Sub btEditQuery_Click(ByVal sender As Object, ByVal e As EventArgs)
+            sqlDataSource1.Queries("DefaultQuery").EditQuery()
             sqlDataSource1.Fill()
-
             gridView1.PopulateColumns()
         End Sub
     End Class
